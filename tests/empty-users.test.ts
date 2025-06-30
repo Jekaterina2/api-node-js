10f656f208676aeda5952b67c5ebaa928c12e124
Jekaterina <mihhailova.jekaterina@gmail.com>
2025-06-30 20:57:40 +0300

    Lesson 11

--- tests/empty-users.test.ts
+++ tests/empty-users.test.ts
@@ -1,14 +1,19 @@
 
 import { test, expect } from '@playwright/test';
 import {StatusCodes} from "http-status-codes";
+import {id_ID} from "@faker-js/faker";
 let baseURL: string = 'http://localhost:3000/users';
-import ( StatusCodes)
 
 test.describe('No users tests', () => {
-    test('GET / - should return empty when no users', async ({ request }) => {
-        const response = await request.get(`${baseURL}`);
-        expect (response.status()).toBe(StatusCodes.BAD_REQUEST);
-        const responseBody = await response.text()
+    test.beforeEach(async ({ request }) => {
+        const res = await request.get(`${baseURL}`);
+        const responseBody = await res.text()
         expect(responseBody).toBe('[]');
+    });
+    test('GET should return empty when no users', async ({ request }) => {
+        const response = await request.get(`${baseURL}`);
+        expect(response.status()).toBe(StatusCodes.OK);
+        const responseBody = await response.text()
+        expect(response.status).toBeTruthy();
     });
 })
